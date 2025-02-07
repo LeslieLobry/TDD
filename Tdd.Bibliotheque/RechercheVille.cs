8d757056bd4267372855deb9e4b5e4b5bc7c7f48
leslie lobry <leslielobry@gmail.com>
2025-02-07 13:54:25 +0100

    initial test  :test_tube:

---
+++ Tdd.Bibliotheque/RechercheVille.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tdd.Bibliotheque
+{
+    public class RechercheVille
+    {
+        private List<String> _villes;
+
+        public List<String> Rechercher(String mot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
